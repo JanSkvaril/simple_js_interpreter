5001d4a96b70a660d480fbaa4814a13258068bd8
DESKTOP-1O3RCUI\Dewid-nb <kondr4@seznam.cz>
2018-12-20 17:34:58 +0100

    basic execution

--- execute.js
+++ execute.js
@@ -1,7 +1,38 @@
 
-//todo
 function execute(data,command){
-    
+    if(command.type == "error") return;
+    else if (command.type == "varDeclaration"){
+        data.variables[command.name.toString()] = execValue(data,command.value);
+    }
+
+    return data;
+}
+
+function execValue(data,value){
+  if (value.type == "number"){
+      return value.value;
+  }
+  else if (value.type == "calculation"){
+      if (value.containsVariable == true){
+       value.value = replaceVar(data,value.value);
+      }
+      return calculate(value.value);
+  }
+  else if (value.type == "variable"){
+      return replaceVar(data,value.value);
+  }
+}
+
+function replaceVar(data,val){
+    let variables = val.match(/[a-zA-Z]+\d*/g);
+ 
+    for (variable of variables) {
+        console.log("variables: " + variable);
+        if (!!data.variables[variable.toString()]){
+           val = val.replace(variable.toString(),data.variables[variable.toString()]);
+        }
+    }
+    return val;
 }
 
 function calculate(value){
@@ -12,3 +43,5 @@ function calculate(value){
     else return eval(value);
 
 }
+
+module.exports = execute;
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let tokenizer = require("./tokenize");
-let code = ' var dd4 = 56 + 6;Abc=5 ; = hi;';
+let execute = require("./execute");
+let code = ' var dd4 = 56 + 3;var Abc=5 + dd4 ;var k = hi;';
 //let result = tokenizer.tokenize(code,1);
-console.log(tokenizer(code));
+let commands = tokenizer(code);
+//console.log(commands[0]);
+let data = {variables:[]};
+console.log(execute(data,commands[0]));
+console.log(execute(data,commands[1]));
